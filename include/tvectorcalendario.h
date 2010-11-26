28c8855bfc0076d17a7641d958b9765c97f07640
javier6989 <javier6989@f1768c50-85fa-8e18-060e-33f9a243e389>
2010-11-26 12:13:28 +0000

    VeKtor casi hexo

---
+++ include/tvectorcalendario.h
@@ -0,0 +1,149 @@
+#ifndef TVECTORCALENDARIO_H_
+#define TVECTORCALENDARIO_H_
+
+#include "tcalendario.h"
+/** \file tvectorcalendario.h
+ *
+ * \brief Estructura de la clase TVectorCalendario.
+ *
+ * Definiciones de métodos y atributos de clase TVectorCalendario.
+ */
+
+
+
+/** \brief Clase TVectorCalendario.
+ *
+ * Estructura de la clase TVectorCalendario.
+ *
+ */
+class TVectorCalendario
+{
+	
+	/** \brief Sobrecarga del operador "<<", función amiga.
+	 *
+	 * Función amiga que se encarga de mostrar por pantalla, en un formato determinado un TVectorCalendario que le pasemos.
+	 *
+	 * \param os Ostream donde guardaremos la informacion que queremos mostrar.
+	 * \param v TVectorCalendario que queremos mostrar.
+	 *
+	 * \return La funcion devuelve un ostream&.
+	 */
+	friend ostream& operator <<(ostream&, const TVectorCalendario&);
+
+public:
+
+	/** \brief Constructor por defecto.
+	 *
+	 * Constructor por defecto que inicializa a 0 los componentes del TVectorCalendario.
+	 */
+	TVectorCalendario();
+
+	/** \brief Constructor sobrecargado.
+	 *
+	 * Constructor sobrecargado que recibe un entero con el que le daremos una dimension al vector.
+	 * \param tam Entero que contiene el valor de la dimensión que queremos asignarle al vector.
+	 */
+	TVectorCalendario( const int);
+
+	/** \brief Constructor de copia.
+	 *
+	 * Constructor de copia de vectores.
+	 * \param v TVectorCalendario desde el que copiamos al nuevo TVectorCalendario.
+	 */
+	TVectorCalendario( const TVectorCalendario&);
+
+	/** \brief Destructor de la clase.
+	 *
+	 * Destructor de la clase, borra la memoria.
+	 */
+	~TVectorCalendario();
+	/** \brief Sobrecarga del operador asignacion.
+	 *
+	 *
+	 * Asignamos el contenido del parámetro al objeto que llama al método.
+	 * \param v TVectorCalendario del cual asignamos.
+	 * \return Devuelve un TVectorCalendario&.
+	 */
+	TVectorCalendario& operator=( const TVectorCalendario&);
+	/** \brief Sobrecarga del operador igualdad.
+	 *
+	 * Devuelve true si los dos TVectorCalendario son iguales.
+	 * \param v TVectorCalendario del cual comprobamos que es igual.
+	 * \return Devuelve un bool.
+	 */
+	bool operator==( const TVectorCalendario&);
+	/** \brief Sobrecarga del operador desigualdad.
+	 *
+	 * Devuelve true si los dos TVectorCalendario son desiguales.
+	 * \param v TVectorCalendario del cual comprobamos que es desigual.
+	 * \return Devuelve un bool.
+	 */
+	bool operator!=( const TVectorCalendario&);
+	/** \brief Sobrecarga del operador corchete derecho.
+	 *
+	 * Inserta un TCalendario en el vector
+	 * \param int posicion donde se guarda el TCalendario.
+	 * \return Devuelve el TCalendario introducido.
+	 */
+	TCalendario & operator[](int);
+	/** \brief Sobrecarga del operador corchete izquierdo.
+	 *
+	 * Devuelve un TCalendario del vector
+	 * \param int posicion donde se encuentra el TCalendario.
+	 * \return Devuelve el TCalendario de la posicion.
+	 */
+	TCalendario operator[](int) const;
+
+	/** \brief Metodo Longitud.
+	 *
+	 * Devuelve un entero con la longitud del vector, que es su tamaño.
+	 * \return Devuelve un entero, dimension del vector.
+	 */
+	int Tamano() const;
+
+	/** \brief Metodo Cantidad.
+	 *
+	 * Este metodo se encarga de contar los Calendarios del vector que no estan vacios.
+	 * \return Devuelve un entero con la cantidad de proos no vacios.
+	 */
+	int Ocupadas() const;
+
+	
+	bool ExisteCal( const TCalendario &);
+	
+	void MostrarMensajes( const int, const int, const int);
+	
+
+
+	/** \brief Metodo Redimensionar.
+	 *
+	 * Este metodo efectúa una redimension del vector, eliminando los componenetes finales si el tamaño que le pasamos es menor que el anterior o creando Calendarios vacios si este es mayor.
+	 * \param tam Entero mediante el cual introduciremos el nuevo valor de dimension del vector.
+	 * \return Devuelve un bool, indica si se ha redimensionado o no.
+	 */
+	bool Redimensionar(int);
+
+private:
+
+	/** \brief Entero tamano.
+	 *
+	 * Contiene el valor del tamaño total del vector TVectorCalendario.
+	 */
+	int tamano;
+
+	/** \brief Puntero a TCalendario.
+	 *
+	 * Con este puntero haremos las veces de vector donde almacenamos TCalendario's.
+	 */
+	TCalendario *c;
+
+	/** \brief TCalendario error.
+	 *
+	 * TCalendario creado por si hay alguna situacion de error devolverlo.
+	 */
+	TCalendario error;
+
+};
+
+
+#endif /* TVECTORCALENDARIO_H_ */
---
+++ lib/tvectorcalendario.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include "tcalendario.h"
+#include "tvectorcalendario.h"
+
+
+
+using namespace std;
+
+
+TVectorCalendario::TVectorCalendario()
+{
+	tamano = 0;
+
+	c = NULL;
+
+
+}
+
+TVectorCalendario::TVectorCalendario( const int dim )
+{
+	if ( dim < 0)
+	{
+		tamano = 0;
+
+		c = NULL;
+	}
+	else
+	{
+		tamano = dim;
+
+		c = new TCalendario[dim];
+	}
+}
+
+TVectorCalendario::TVectorCalendario( const TVectorCalendario & v )
+{
+
+
+	if( v.tamano == 0 )
+	{
+		tamano = 0;
+		c = NULL;
+	}
+	else
+	{
+		tamano = v.tamano;
+		c = new TCalendario[ tamano ];
+
+		for( int i = 0; i < tamano; i++)
+		{
+			c[i] = v.c[i];
+		}
+	}
+}
+
+TVectorCalendario::~TVectorCalendario()
+{
+	delete [] c;
+	c = NULL;
+	tamano = 0;
+}
+
+TVectorCalendario&
+TVectorCalendario::operator =( const TVectorCalendario &v)
+{
+	if( this != &v)
+	{
+
+		if( v.tamano != 0 )
+		{
+			tamano = v.tamano;
+
+			c = new TCalendario[tamano];
+
+			for( int i = 0; i < tamano; i++)
+			{
+				c[i] = v.c[i];
+			}
+		}
+		else
+		{
+			tamano = 0;
+			c = NULL;
+		}
+	}
+	return *this;
+}
+
+
+bool
+TVectorCalendario::operator ==( const TVectorCalendario &v)
+{
+	bool igual = true;
+
+	if ( tamano == v.tamano )
+	{
+		for( int i = 0; i < tamano; i++)
+		{
+			if( c[i] != v.c[i] )
+				igual = false;
+		}
+	}
+	else
+		igual = false;
+
+	return igual;
+}
+bool
+TVectorCalendario::operator !=( const TVectorCalendario &v)
+{
+	bool distinto = false;
+
+	if ( tamano == v.tamano )
+	{
+		for( int i = 0; i < tamano; i++)
+		{
+			if( c[i] != v.c[i] )
+				distinto = true;
+		}
+	}
+	else
+		distinto = true;
+
+	return distinto;
+}
+
+TCalendario&
+TVectorCalendario::operator [](int pos)
+{
+
+	TCalendario *dato = NULL;
+
+	if ( pos >= 1 && pos <= tamano)
+	{
+		dato = &c[pos-1];
+	}
+	else
+	{
+		dato = &error;
+	}
+
+	return *dato;
+}
+
+TCalendario
+TVectorCalendario::operator[] ( int pos ) const
+{
+	if ( pos >= 1 && pos <= tamano)
+	{
+		return c[pos-1];
+	}
+	else
+	{
+		return error;
+	}
+}
+
+int
+TVectorCalendario::Tamano() const
+{
+	return tamano;
+}
+
+int
+TVectorCalendario::Ocupadas() const
+{
+	int cont = 0;
+
+	for( int i = 0; i < tamano; i++)
+	{
+		if ( !c[i].EsVacio() )
+			cont ++;
+	}
+
+	return cont;
+}
+
+bool
+TVectorCalendario::ExisteCal( const TCalendario &calen)
+{
+	bool existe = false;
+	
+	for( int i = 0; i < tamano && !existe; i++)
+	{
+		if ( c[i] == calen )
+			existe = true;
+	}
+
+	return existe;
+}
+
+void MostrarMensajes(int d, int m, int a )
+{
+
+}
+
+bool
+TVectorCalendario::Redimensionar(int tam)
+{
+	bool redi = false;
+	int i = 0;
+
+	if ( tam > 0 )
+	{
+		if(tam != tamano)
+		{
+			TVectorCalendario aux(tam);
+			redi = true;
+
+			if ( tam > tamano)
+			{
+
+				for ( ; i < tamano; i++)
+				{
+					aux.c[i] = this->c[i];
+				}
+
+			}
+			else
+			{
+				//Vector nuevo de tamaño menor al antiguo.
+				for( ; i < tam; i++)
+				{
+					aux.c[i] = c[i];
+				}
+
+
+			}
+
+			 *this = aux;
+		}
+	}
+	return redi;
+}
+
+ostream&
+operator<<( ostream& os, const TVectorCalendario &v )
+{
+	int i = 0;
+
+	os << "[";
+	if ( v.Tamano() > 0 )
+	{
+		os << "("<< i+1 << ") "<< v.c[i];
+		i++;
+
+		for( ; i < v.Tamano(); i++)
+		{
+			os << ", " << i+1 << v.c[i];
+		}
+	}
+
+	os << "]";
+
+	return os;
+}
+
+
