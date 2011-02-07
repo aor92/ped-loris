51a062875bab1e2cac9d7679d6b6ba38fdd36179
javier6989 <javier6989@f1768c50-85fa-8e18-060e-33f9a243e389>
2011-02-07 09:19:32 +0000

    Algo mas

--- lib/tabbcalendario.cpp
+++ lib/tabbcalendario.cpp
@@ -1,2 +1,48 @@
 #unclude "tabbcalendario.h"
 #include "tcalendario.h"
+
+
+TNodoABB::TNodoABB()
+{
+	iz = de = NULL;
+}
+
+TNodoABB::TNodoABB( const TNodoABB &nodo )
+{
+	item = nodo.item;
+	
+	if( nodo.iz != NULL) iz = nodo.iz;
+	if( nodo.de != NULL) de = nodo.de;
+}
+
+TNodoABB::~TNodoABB()
+{
+	if( iz != NULL ) delete iz;
+	if( de != NULL ) delete de;
+	iz = de = NULL;
+}
+
+TNodoABB&
+TNodoABB::operator =(const TNodoABB &nodo )
+{
+	if( nodo != &this )
+	{
+		item = nodo.item;
+		de = nodo.de;
+		iz = nodo.iz;
+	}
+}
+
+	//////////////////////////////////////////////////////////////////////
+///////////////////////////ABBCALENDARIO//////////////////////////////////////  *3* 
+	//////////////////////////////////////////////////////////////////////
+
+TABBCalendario::TABBCalendario()
+{
+	raiz = NULL;
+}
+
+TABBCalendario::~TABBCalendario()
+{
+	
+}
