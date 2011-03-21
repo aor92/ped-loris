432cb700319a18e3f59c1e8e8b8f40c52a4cd2bb
Arkariang <Arkariang@f1768c50-85fa-8e18-060e-33f9a243e389>
2011-03-21 09:26:36 +0000

    go on

--- lib/tavlcalendario.cpp
+++ lib/tavlcalendario.cpp
@@ -95,3 +95,134 @@ TAVLCalendario::~TAVLCalendario()
 		raiz = NULL;
 	}
 }
+
+bool 
+TAVLCalendario::operator==( TAVLCalendario &)const 
+{
+	if((raiz!=NULL && der.raiz==NULL) || (raiz==NULL && der.raiz!=NULL))
+	{
+		return false;
+	}
+	else if(raiz==NULL && der.raiz==NULL) return true;
+	else if(raiz->item!=der.raiz->item) return false;
+	else return(raiz->de==der.raiz->de && raiz->iz==der.raiz->iz);	
+}
+
+bool 
+TAVLCalendario::operator!=( TAVLCalendario & a)const 
+{
+	return !(this==a);
+}
+
+bool
+TAVLCalendario::EsVacio() const
+{
+	return raiz==NULL;
+}
+
+int 
+TAVLCalendario::Nodos() const
+{
+	int num=0;
+		if (raiz!=NULL)
+		{
+			num=1;
+			if(!(raiz->iz).EsVacio()) num=num+raiz->iz.Nodos();
+			if(!(raiz->de).EsVacio()) num=num+raiz->de.Nodos();
+		}
+	return num;
+}
+
+int 
+TAVLCalendario::NodosHoja()const
+{
+	if (EsVacio() && raiz->iz.EsVacio() && raiz->de.EsVacio() )
+	{
+		return 1;
+	}
+	else raiz->iz.NodosHoja() + raiz->de.NodosHoja();
+}
+
+int
+TAVLCalendario::Altura() const
+{
+	int hd=0;
+	int hi=0;
+	
+	if (EsVacio())
+	{
+		return hd+hi;
+	}
+	else
+	{
+		hd+=1+raiz->de.Altura();
+		hi+=1+raiz->iz.Altura();
+	}
+	
+}
+
+
+TVectorCalendario 
+TAVLCalendario::Inorden() const
+{
+	int pos=1;
+	TVectorCalendario v(Nodos());
+	InordenAux(v,pos);
+	return v;
+}
+
+void
+TAVLCalendario::InordenAux(TVectorCalendario& v, int& pos) const
+{
+	if(!EsVacio())
+	{
+		raiz->iz.InordenAux(v,pos);
+		v[pos]=Raiz();
+		pos++;
+		raiz->de.InordenAux(v,pos);
+	}
+}
+
+TVectorCalendario
+TAVLCalendario::Preorden() const
+{
+	int pos=1;
+	TVectorCalendario v(Nodos());
+	PreordenAux(v,pos);
+	return v;
+}
+
+void
+TAVLCalendario::PreordenAux(TVectorCalendario& v, int& pos)const 
+{
+	if(!EsVacio())
+	{
+		v[pos]=Raiz();
+		pos++;
+		raiz->iz.PreordenAux(v,pos);
+		raiz->de.PreordenAux(v,pos);
+	}
+}
+
+TVectorCalendario
+TAVLCalendario::Postorden() const
+{
+	int pos=1;
+	TVectorCalendario v(Nodos());
+	PostordenAux(v,pos);
+	return v;
+}
+
+void
+TAVLCalendario::PostordenAux(TVectorCalendario& v, int& pos) const
+{
+	if(!EsVacio())
+	{
+		raiz->iz.PostordenAux(v,pos);
+		raiz->de.PostordenAux(v,pos);
+		v[pos]=Raiz();
+		pos++;
+	}
+}
+
+
