cf6e855b00eb081b8d13ad34000e9d02640d4559
javier6989 <javier6989@f1768c50-85fa-8e18-060e-33f9a243e389>
2011-05-23 08:04:58 +0000

    Algo ocmentado

--- include/tavlcalendario.h
+++ include/tavlcalendario.h
@@ -1,6 +1,13 @@
 #ifndef __TAVLCALENDARIO_H
 #define __TAVLCALENDARIO_H
 
+/** \file tavlcalendario.h
+* 	\brief Este fichero contiene la clase tavlcalendario
+*
+* 	En el fichero se incluyen los encabezados de las funciones de la clase tcalendario asi como miembros y 
+* 	declaraciones
+*/
+
 #include <iostream>
 #include "tcalendario.h"
 #include "tvectorcalendario.h"
@@ -11,62 +18,148 @@ using namespace std;
 
 class TNodoAVL;
 
+/** \brief Clase tavlcalendario
+*
+* 	La clase tavlcalendario es un clase que contiene un arbol balanceado de tcalendarios
+*/
 class TAVLCalendario
 {
 	friend class TNodoAVL;
 	friend ostream& operator<<(ostream&,const TAVLCalendario&);
 	private:
 		
+		/** \brief raiz
+		* Raiz del arbol AVL*/
 		TNodoAVL *raiz;
 		
-		// Devuelve el recorrido en inorden
+		/** \brief Auxiliar de Inorden
+		*  Asigna las posiciones al vector siguiendo el algoritmo inorden
+		*  \param v Vector donde almacenamos el nuevo orden
+		* 	\param pos entero que marca la posicion en el vector
+		*/
 		void InordenAux(TVectorCalendario &, int&) const;
-		// Devuelve el recorrido en preorden
+		
+		/** \brief Auxiliar de Preorden
+		*  Asigna las posiciones al vector siguiendo el algoritmo preorden
+		*  \param v Vector donde almacenamos el nuevo orden
+		* 	\param pos entero que marca la posicion en el vector
+		*/
 		void PreordenAux(TVectorCalendario &, int&) const;
-		// Devuelve el recorrido en postorden
+		
+		/** \brief Auxiliar de Preorden
+		*  Asigna las posiciones al vector siguiendo el algoritmo postorden
+		*  \param v Vector donde almacenamos el nuevo orden
+		* 	\param pos entero que marca la posicion en el vector
+		*/
 		void PostordenAux(TVectorCalendario &, int&) const;
 		
+		/** \brief Funcion auxiliar para el borrado Eliminar
+		*  Borra el contenido del arbol de forma recursiva
+		*  \param n Nodo con el que recorremos el arbol a borrar
+		*/
 		void Eliminar( TNodoAVL &);
 		
+		/** \brief Funcion auxiliar para la copia Copiar
+		*  Copia el contenido del arbol de forma recursiva
+		*  \param acopiar Arbol que queremos copiar
+		*/
 		void Copiar(const TAVLCalendario & );
 		
+		/** \brief Metodo para equilibrar el arbol siguiendo el comportamiento AVL
+		*  Equilibra el arbol
+		*/
 		void Equilibrar();
 		
 	
 	public:
 	
-		// Constructor por defecto
+		/** \brief Constructor por defecto.
+		 *
+		 * Constructor por defecto que inicializa según el formato especificado los componentes del TavlCalendario.
+		 */
 		TAVLCalendario();
-		// Constructor de copia
+		
+		/** \brief Constructor de copia.
+		 *
+		 * Constructor de copia de TAVLCalendario.
+		 * \param avl TAVLCalendario desde el que copiamos al nuevo TAVLCalendario.
+		 */
 		TAVLCalendario(const TAVLCalendario &);
-		// Destructor
+		
+		/** \brief Destructor de TAVLCalendario
+		*   Destruye el arbol
+		*/
 		~ TAVLCalendario ();
-		// Sobrecarga del operador asignación
+		
+		/** \brief Operador de asignacion de la clase AVLCalendario
+		*  Asigna los valores de un AVLcalendario a otro
+		*  \param avl AVL desde el que se asigna
+		*/
 		TAVLCalendario& operator=(const TAVLCalendario &);
-		// MÉTODOS de TAVLCalendario
-		// Sobrecarga del operador igualdad
+		
+		/** \brief Operador comparacion(igualdad) de AVL's
+		*  Devuelve true si el contenido de los dos arboles es el mismo
+		*  \param de AVLcalendario de la derecha de la comparación
+		*/
 		bool operator==( const TAVLCalendario &) const;
-		// Sobrecarga del operador desigualdad
+		
+		/** \brief Operador comparacion(distinto) de AVL's
+		*  Devuelve true si el contenido de los arboles no es el mismo
+		*  \param a calendario de la derecha de la comparación
+		*/
 		bool operator!=( const TAVLCalendario &) const;
-		// Devuelve TRUE si el árbol está vacío, FALSE en caso contrario
+		
+		/** \brief Metodo EsVacio()
+		*  Devuelve true si el arbol no contiene ningun elemento
+		*/
 		bool EsVacio() const;
-		// Inserta el elemento en el árbol
+		
+		/** \brief Metodo Insertar de AVL
+		*  Introduce el elemento pasado al metodo en el arboll de forma ordenada. Nos devuelve un bool
+		*  \param c Calendario a insertar en el arbol
+		*/
 		bool Insertar(const TCalendario &);
-		// Devuelve true si el elemento está en el árbol, false en caso contrario
+		
+		/** \brief Metodo Buscar de AVL
+		* 	Busca un Calendario en el AVL que llama al metodo. Devuelve un bool
+		*  \param obj Calendario que queremos buscar en el AVL
+		*/
 		bool Buscar( const TCalendario&);
-		// Devuelve la altura del árbol (la altura de un árbol vacío es 0)
+		
+		/** \brief Metodo Altura de AVL
+		*  Devuelve un entero indicando la altura que tiene el AVL en ese momento.
+		*/
 		int Altura() const ;
-		// Devuelve el número de nodos del árbol (un árbol vacío posee 0 nodos)
+		
+		/** \brief Metodo Nodos de AVL
+		*  Devuelve un entero con el numero de Nodos del arbol.
+		*/
 		int Nodos() const;
-		// Devuelve el número de nodos hoja en el árbol (la raíz puede ser nodo hoja)
+		
+		/** \brief Metodo Nodos de AVL
+		*  Devuelve un entero con el numero de Nodos hoja del arbol.
+		*/
 		int NodosHoja() const;
-		// Devuelve el recorrido en inorden sobre un vector
+		
+		/** \brief Metodo Inorden de AVL
+		*  Devuelve un vector con el contenido del arbol siguiendo el esquema inorden
+		*/
 		TVectorCalendario Inorden() const;
-		// Devuelve el recorrido en preorden sobre un vector
+		
+		/** \brief Metodo Preorden de AVL
+		*  Devuelve un vector con el contenido del arbol siguiendo el esquema Preorden
+		*/
 		TVectorCalendario Preorden() const;
-		// Devuelve el recorrido en postorden sobre un vector
+		
+		/** \brief Metodo Postorden de AVL
+		*  Devuelve un vector con el contenido del arbol siguiendo el esquema Postorden
+		*/
 		TVectorCalendario Postorden() const;
-		// Borra un TCalendario del árbol AVL
+		
+		/** \brief Metodo Borrar de AVL
+		*  Devuelve bool, borra el calendario que le pasamos por argumento si este existe en el AVL
+		*  \param obj Calendario que queremos borrar en el AVL
+		*/
 		bool Borrar( const TCalendario &);
 		// Devuelve el elemento TCalendario raíz del árbol AVL
 		TCalendario Raiz() const;
